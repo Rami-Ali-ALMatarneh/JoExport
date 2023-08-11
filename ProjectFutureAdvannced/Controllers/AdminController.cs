edf2b7cde3eb8563c0f1a58df8025049b587b8dc
hp <hp@LAPTOP-DH29EFJI>
2023-08-11 03:48:18 +0300

    Last update 11/8/2023

--- ProjectFutureAdvannced/Controllers/AdminController.cs
+++ ProjectFutureAdvannced/Controllers/AdminController.cs
@@ -9,13 +9,13 @@ using ProjectFutureAdvannced.ViewModels;
 using ProjectFutureAdvannced.ViewModels.AdminViewModel;
 
 namespace ProjectFutureAdvannced.Controllers
-{
-    public class AdminController : Controller
     {
+    public class AdminController : Controller
+        {
         private readonly UserManager<Account> _userManager;
         private readonly SignInManager<Account> _signInManager;
         private readonly RoleManager<IdentityRole> _roleManager;
-        private readonly IAdminRepository adminRepository; 
+        private readonly IAdminRepository adminRepository;
         private readonly IWebHostEnvironment webHostEnvironment;
 
         public AdminController( IWebHostEnvironment webHostEnvironment, IAdminRepository adminRepository, UserManager<Account> userManager, SignInManager<Account> signInManager, RoleManager<IdentityRole> _roleManager )
@@ -23,27 +23,26 @@ namespace ProjectFutureAdvannced.Controllers
             this._userManager = userManager;
             this._signInManager = signInManager;
             this._roleManager = _roleManager;
-            this.adminRepository= adminRepository;
+            this.adminRepository = adminRepository;
             this.webHostEnvironment = webHostEnvironment;
             }
         [AllowAnonymous]
         public IActionResult Index()
-        {
-            
+            {
+
             return View();
-        }
+            }
         public IActionResult table()
-        {
+            {
             return View();
-        }
+            }
         [HttpGet]
-        public async Task<IActionResult> profile()
+        public async Task<IActionResult> GeneralProfile()
             {
             ListOfInfoAdmin model;
             var user = await _userManager.GetUserAsync(User);
             if (user != null)
                 {
-
                 var admin = adminRepository.GetByFK(user.Id);
                 model = new ListOfInfoAdmin()
                     {
@@ -57,45 +56,53 @@ namespace ProjectFutureAdvannced.Controllers
                     };
                 return View(model);
                 }
-            return RedirectToAction("Index","Home");
+            return RedirectToAction("Index", "Home");
             }
         /*************************************/
-        //[HttpPost]
-        //public async Task<IActionResult> profile()
-        //    {
-        //    //if(ModelState.IsValid)
-        //    //    {
-        //    //    if(admin.ImgUser != null)
-        //    //        {
-        //    //        var folder = "/AccountImg/";
-        //    //        folder += admin.ImgUser.FileName + "_" + Guid.NewGuid().ToString();
-        //    //        admin.UrlImgString = folder;
-        //    //        string ServerFolder = Path.Combine(webHostEnvironment.WebRootPath, folder);
-        //    //        await admin.ImgUser.CopyToAsync(new FileStream(ServerFolder, FileMode.Create));
-        //    //        }
-        //    //    /*********************/
-        //    //    int indexOfAt = admin.Email.IndexOf("@");
-        //    //    var user = await _userManager.GetUserAsync(User);
-        //    //    user.Name=admin.Name;
-        //    //    user.UserName = admin.Email.Substring(0, indexOfAt - 1);
-        //    //    user.Email = admin.Email;
-        //    //    user.Password = admin.Password;
-        //    //    user.ConfirmPassword = admin.ConfirmPassword;
-        //    //    /*************************/
-        //    //    Admin admin1 = new Admin()
-        //    //        {
-        //    //        Name = admin.Name,
-        //    //        Email = admin.Email,
-        //    //        Password = admin.Password,
-        //    //        ConfirmPassword = admin.ConfirmPassword,
-        //    //        TypeOfRoles = admin.TypeOfRoles,
-        //    //        ImgUrl = admin.UrlImgString,
-        //    //        };
-        //    //         adminRepository.Update(admin1);
-        //    //    await _userManager.UpdateAsync(user);
-        //    //    }
-        //    return View();  
+        [HttpPost]
+        public async Task<IActionResult> GeneralProfile( GeneralInfoAdmin admin )
+            {
+            if (ModelState.IsValid)
+                {
+                if (admin.ImgUser != null)
+                    {
+                    var folder = "/AccountImg/";
+                    folder += admin.ImgUser.FileName + "_" + Guid.NewGuid().ToString();
+                    admin.UrlImgString = folder;
+                    string ServerFolder = Path.Combine(webHostEnvironment.WebRootPath, folder);
+                    await admin.ImgUser.CopyToAsync(new FileStream(ServerFolder, FileMode.Create));
+                    }
+                /*********************/
+                int indexOfAt = admin.Email.IndexOf("@");
+                var user = await _userManager.GetUserAsync(User);
+                user.Name = admin.Name;
+                user.UserName = admin.Email.Substring(0, indexOfAt - 1);
+                user.Email = admin.Email;
+                /*************************/
+                Admin admin1 = new Admin()
+                    {
+                    Name = admin.Name,
+                    Email = admin.Email,
+                    TypeOfRoles = admin.TypeOfRoles,
+                    ImgUrl = admin.UrlImgString,
+                    };
+                adminRepository.Update(admin1);
+                await _userManager.UpdateAsync(user);
+                ListOfInfoAdmin listOfInfoAdmin = new ListOfInfoAdmin()
+                    {
+                    GeneralInfoAdmin=new GeneralInfoAdmin()
+                        {
+                        Name = admin.Name,
+                        Email=admin.Email,
+                        TypeOfRoles = admin.TypeOfRoles,
+                        UrlImgString= admin.UrlImgString,
+                        }
+                    };
+                return View(listOfInfoAdmin);
+                }
+            return View();
             }
         }
+    }
 
 
